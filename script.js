a2dbe26d65e6b0bfab904e8e9d2e2620d0d152b8
kenneth adutwum <kwamayadu@gmail.com>
2023-08-14 20:44:19 +0000

    Update in script.js

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
-// const addBookButton = document.querySelector('#add-book');
-// const bookList = document.querySelector('.book-list');
-
-// const libBooks = [];
-// addBookButton.addEventListener('click', () => {
-//   const bookTitle = document.querySelector('#book-title').value;
-//   const authorName = document.querySelector('#author-name').value;
-//   const book = {'book':bookTitle, 'author':authorName};
-//   libBooks.push(book);
-
-//   if (bookTitle && authorName) {
-//     const bookItem = document.createElement('p');
-//     bookItem.innerHTML = `${bookTitle}<br>   by  <br> ${authorName}`;
-
-//     const divider = document.createElement('hr');
-
-//     const removeBook = document.createElement('button');
-//     removeBook.innerText = 'Remove';
-//     removeBook.style.display = 'block';
-
-//     removeBook.addEventListener('click', () => {
-//       bookItem.remove();
-//     });
-
-//     bookItem.appendChild(removeBook);
-//     bookItem.appendChild(divider);
-//     bookList.appendChild(bookItem);
-
-//     document.querySelector('#book-title').value = '';
-//     document.querySelector('#author-name').value = '';
-//   }
-// });
-// console.log(libBooks);
 // create empty list [{book:title, author:athorName}]
 // put in localStorage
 // fetch from local storage
 // display to user
 // remove from list and update localStorage
+
+
 
 const addBookButton = document.querySelector('#add-book');
 const bookList = document.querySelector('.book-list');
@@ -45,13 +14,15 @@ const libBooks = JSON.parse(localStorage.getItem('libraryBooks')) || [];
 function updateLocalStorage() {
   localStorage.setItem('libraryBooks', JSON.stringify(libBooks));
 }
-
+// Grabbing the value of the books and the author when the addBookButton is clicked
 addBookButton.addEventListener('click', () => {
   const bookTitle = document.querySelector('#book-title').value;
   const authorName = document.querySelector('#author-name').value;
 
+  // check if bookTitle and authorName are present
   if (bookTitle && authorName) {
-    const book = { book: bookTitle, author: authorName };
+    // create an object of the book and author
+    const book = {'book': bookTitle, 'author': authorName};
     libBooks.push(book);
     updateLocalStorage();
 
@@ -65,7 +36,8 @@ addBookButton.addEventListener('click', () => {
     removeBook.style.display = 'block';
 
     removeBook.addEventListener('click', () => {
-      const index = libBooks.findIndex((b) => b.book === bookTitle && b.author === authorName);
+      // Romoving the book from localStorage
+      const index = libBooks.findIndex(b => b.book === bookTitle && b.author === authorName);
       if (index !== -1) {
         libBooks.splice(index, 1);
         updateLocalStorage();
@@ -82,7 +54,7 @@ addBookButton.addEventListener('click', () => {
 });
 
 // Fetch books from local storage and display them
-libBooks.forEach((book) => {
+libBooks.forEach(book => {
   const bookItem = document.createElement('p');
   bookItem.innerHTML = `${book.book}<br>   by  <br> ${book.author}`;
 
@@ -105,3 +77,4 @@ libBooks.forEach((book) => {
   bookItem.appendChild(divider);
   bookList.appendChild(bookItem);
 });
+
