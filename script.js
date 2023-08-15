54a9812f6c905ead6c35a1e3ed3ac9d47f9075b4
ismail halliru muhammad <ismailhm888@gmail.com>
2023-08-15 09:41:14 +0100

    Update script.js file

--- script.js
+++ script.js
@@ -4,22 +4,22 @@
 // display to user
 // remove from list and update localStorage
 
-const addBookButton = document.querySelector("#add-book");
-const bookList = document.querySelector(".book-list");
-const libBooks = JSON.parse(localStorage.getItem("libraryBooks")) || [];
+const addBookButton = document.querySelector('#add-book');
+const bookList = document.querySelector('.book-list');
+const libBooks = JSON.parse(localStorage.getItem('libraryBooks')) || [];
 
 // Function to update local storage
 function updateLocalStorage() {
-  localStorage.setItem("libraryBooks", JSON.stringify(libBooks));
+  localStorage.setItem('libraryBooks', JSON.stringify(libBooks));
 }
 // Grabbing the value of the books and the author when the addBookButton is clicked
-addBookButton.addEventListener("click", (e) => {
-  const bookTitle = document.querySelector("#book-title").value;
-  const authorName = document.querySelector("#author-name").value;
-  const message = document.querySelector(".message");
+addBookButton.addEventListener('click', (e) => {
+  const bookTitle = document.querySelector('#book-title').value;
+  const authorName = document.querySelector('#author-name').value;
+  const message = document.querySelector('.message');
 
-  if (bookList === "" || authorName === "") {
-    message.innerText = "Please input Title and Author";
+  if (bookList === '' || authorName === '') {
+    message.innerText = 'Please input Title and Author';
     e.preventDefault();
   }
 
@@ -30,20 +30,20 @@ addBookButton.addEventListener("click", (e) => {
     libBooks.push(book);
     updateLocalStorage();
 
-    const bookItem = document.createElement("p");
+    const bookItem = document.createElement('p');
     bookItem.innerHTML = `${bookTitle}<br>   by  <br> ${authorName}`;
 
-    const divider = document.createElement("hr");
+    const divider = document.createElement('hr');
 
-    const removeBook = document.createElement("button");
-    removeBook.innerText = "Remove";
+    const removeBook = document.createElement('button');
+    removeBook.innerText = 'Remove';
 
-    removeBook.className = "rmbook";
+    removeBook.className = 'rmbook';
 
-    removeBook.addEventListener("click", () => {
+    removeBook.addEventListener('click', () => {
       // Romoving the book from localStorage
       const index = libBooks.findIndex(
-        (b) => b.book === bookTitle && b.author === authorName
+        (b) => b.book === bookTitle && b.author === authorName,
       );
       if (index !== -1) {
         libBooks.splice(index, 1);
@@ -54,23 +54,23 @@ addBookButton.addEventListener("click", (e) => {
     bookItem.append(removeBook, divider);
     bookList.append(bookItem);
 
-    document.querySelector("#book-title").value = "";
-    document.querySelector("#author-name").value = "";
+    document.querySelector('#book-title').value = '';
+    document.querySelector('#author-name').value = '';
   }
 });
 
 // Fetch books from local storage and display them
 libBooks.forEach((book) => {
-  const bookItem = document.createElement("p");
+  const bookItem = document.createElement('p');
   bookItem.innerHTML = `${book.book}<br>   by  <br> ${book.author}`;
 
-  const divider = document.createElement("hr");
+  const divider = document.createElement('hr');
 
-  const removeBook = document.createElement("button");
-  removeBook.innerText = "Remove";
-  removeBook.style.display = "block";
+  const removeBook = document.createElement('button');
+  removeBook.innerText = 'Remove';
+  removeBook.style.display = 'block';
 
-  removeBook.addEventListener("click", () => {
+  removeBook.addEventListener('click', () => {
     const index = libBooks.indexOf(book);
     if (index !== -1) {
       libBooks.splice(index, 1);
