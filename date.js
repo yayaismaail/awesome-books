8c21ba9705b23a5601eda8d13cd00a7337ab5175
kenneth adutwum <kwamayadu@gmail.com>
2023-08-17 01:03:27 +0000

    Fix Navigation

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-const date = document.querySelector(".date");
+const date = document.querySelector('.date');
 const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth() + 1;
@@ -7,6 +7,6 @@ const hour = today.getHours();
 const minutes = today.getMinutes();
 const seconds = today.getSeconds();
 const period = today.getTime();
-const current_date = `${day}/${month}/${year}| ${hour}:${minutes}:${seconds} `;
+const currentDate = `${day}/${month}/${year}| ${hour}:${minutes}:${seconds} `;
 
-date.innerHTML = current_date;
+date.innerHTML = currentDate;
--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 /* eslint-disable no-unused-vars */
 class LibraryApp {
   constructor() {
-    this.addBookButton = document.querySelector("#add-book");
-    this.bookList = document.querySelector(".book-list");
-    this.libBooks = JSON.parse(localStorage.getItem("libraryBooks")) || [];
+    this.addBookButton = document.querySelector('#add-book');
+    this.bookList = document.querySelector('.book-list');
+    this.libBooks = JSON.parse(localStorage.getItem('libraryBooks')) || [];
 
     this.libBooks.forEach((book) => this.displayBook(book));
   }
 
   updateLocalStorage() {
-    localStorage.setItem("libraryBooks", JSON.stringify(this.libBooks));
+    localStorage.setItem('libraryBooks', JSON.stringify(this.libBooks));
   }
 
   handleAddBook(e) {
     e.preventDefault();
 
-    const bookTitle = document.querySelector("#book-title").value;
-    const authorName = document.querySelector("#author-name").value;
-    const display = document.querySelector(".display");
+    const bookTitle = document.querySelector('#book-title').value;
+    const authorName = document.querySelector('#author-name').value;
+    const display = document.querySelector('.display');
 
-    if (bookTitle === "" || authorName === "") {
-      display.classList.add("message");
-      display.innerText = "Please Enter Title and Author";
+    if (bookTitle === '' || authorName === '') {
+      display.classList.add('message');
+      display.innerText = 'Please Enter Title and Author';
 
       return;
     }
 
-  const currentDate = document.querySelector('.date');
-  setInterval(() => {
-  const d = new Date();
-  currentDate.innerHTML = `${d.toDateString()} ${d.toLocaleTimeString()}`;
-}, 1000);
+    const currentDate = document.querySelector('.date');
+    setInterval(() => {
+      const d = new Date();
+      currentDate.innerHTML = `${d.toDateString()} ${d.toLocaleTimeString()}`;
+    }, 1000);
 
     const book = { book: bookTitle, author: authorName };
     this.libBooks.push(book);
     this.updateLocalStorage();
     this.displayBook(book);
 
-    document.querySelector("#book-title").value = "";
-    document.querySelector("#author-name").value = "";
+    document.querySelector('#book-title').value = '';
+    document.querySelector('#author-name').value = '';
   }
 
   displayBook(book) {
-    const div = document.createElement("div");
-    div.className = "display-books";
-    const bookItem = document.createElement("p");
-    bookItem.className = "display-item";
+    const div = document.createElement('div');
+    div.className = 'display-books';
+    const bookItem = document.createElement('p');
+    bookItem.className = 'display-item';
     bookItem.innerHTML = `${book.book} by ${book.author}`;
 
-    const removeBook = document.createElement("button");
-    removeBook.innerText = "Remove";
-    removeBook.className = "rmv-btn";
+    const removeBook = document.createElement('button');
+    removeBook.innerText = 'Remove';
+    removeBook.className = 'rmv-btn';
 
-    removeBook.addEventListener("click", () => {
+    removeBook.addEventListener('click', () => {
       const index = this.libBooks.indexOf(book);
       if (index !== -1) {
         this.libBooks.splice(index, 1);
@@ -68,7 +68,7 @@ class LibraryApp {
 }
 
 const libraryApp = new LibraryApp();
-const button = document.querySelector("#add-book");
-button.addEventListener("click", (e) => {
+const button = document.querySelector('#add-book');
+button.addEventListener('click', (e) => {
   libraryApp.handleAddBook(e);
 });
